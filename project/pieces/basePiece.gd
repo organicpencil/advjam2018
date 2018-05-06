c6d7cb642d00a5be6d77aefa527082d7d7fb5b5c
BLaDoM <simplybladom@gmail.com>
2018-05-06 12:48:50 -0300

    Add files via upload
    Basic Piece and Tiles

--- project/pieces/basePiece.gd
+++ project/pieces/basePiece.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 enum CHAR_TRAITS {
-	QUICK_THINKING, FAST_AIM, LEADER}
+	NONE, QUICK_THINKING, FAST_AIM, LEADER}
+
+export var tilemapPosition = Vector2(0, 0) # Grid Position
 
 export var maxHP = 0
 export var hp = 0
@@ -12,19 +14,25 @@ export var dex = 0 # DEXTERITY 		Magic and stuff
 export var sta = 0 # STAMINA 		Allows you to carry and wield more and heavier equipment
 export var wsm = 0 # WISDOM 		Allows you to wield and use higher level equipment
 
-export var traits = []
+# Trait Dictionary	TRAIT : INTENSITY
+export var traits = {NONE : 0}
 # Traits are special character conditions, this can be anything
 # from more chance to crit to a random chance to move an extra tile
 # in any given turn or even give a small boost to surrounding companions,
 # these traits are defined by the CHAR_TRAITS enum, their classes and
-# properties, any given piece can have as many traits as necessary.
+# properties, any given piece can have as many traits as necessary, this var
+# is a Dictionary, with the first element being the trait itself and the second
+# being a multiplier (where applicable).
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	position = $"..".map_to_world(tilemapPosition)
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _change_pos(newPos):
+	tilemapPosition = newPos
+	position = $"..".map_to_world(tilemapPosition)
+	pass
+
+func _process(delta):
+	
+	pass
