c9a2ddf8af357b97ab2a9eadd43d684e6d24041e
SimplyBLG <simplybladom@gmail.com>
2018-05-06 17:43:26 -0300

    Removed duplicates

--- advjam2018/project/BaseTile.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-enum CUSTOMEFFECTS {
-	NONE, WATER, STICKY_SURFACE, TOXIC_SURFACE, SPIKE_TRAP, TOXIC_TRAP}
-
-export var solid = false
-export var walkable = true
-export var customEffects = [CUSTOMEFFECTS.NONE]
