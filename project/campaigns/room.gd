0a075800581e8625425e053b07d8515fb684e45b
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-05-14 19:26:19 -0500

    Level design friendliness refactors
    -Play musical chairs with various nodes/files
    -Remove camera zoom, scaled a Node2D instead
    -Change Temp from Node to Node2D so it'd inherit scale
    -Better tilemap template
    -Remove tilemapPosition export var and snap to grid based on starting position

--- project/campaigns/room.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-export(PackedScene) var first_encounter
-
-func _ready():
-	assert(first_encounter != null)
-	var scene = first_encounter.instance()
-	add_child(scene)
--- project/tempManager.gd
+++ project/tempManager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 func _flush_temporary_nodes():
 	for i in get_children():
