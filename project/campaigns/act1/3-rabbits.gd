07ce7a323fda1bb8a351511361b851e7e235335e
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-05-17 11:24:25 -0500

    Stubs for rabbit encounter

--- project/campaigns/act1/3-rabbits.gd
+++ project/campaigns/act1/3-rabbits.gd
@@ -1,18 +1,26 @@
 extends Node
 
 onready var hud = $HUD
-var NEXT_ENCOUNTER = preload("res://campaigns/act1/3-rabbits.tscn")
+var NEXT_ENCOUNTER = preload("res://campaigns/act1/0-exposition.tscn")
 
 func _ready():
 	start_encounter()
+	
+func start_combat():
+	hud.hide_message()
+	get_parent().initiate_turn_loop()
+	
+func combat_finished():
+	pass
 
 func start_encounter():
+	print("FIXME - Need signal for combat finish")
 	#get_parent().connect("insert_signal_for_combat_finished_here", self, "combat_finished")
 	# Wait for message_clicked when more dialogue is queued
-	hud.message(hud.AVATAR_DM, "Putting the tavern behind you, you follow the tip on <name here>. Arrive at place. Stuff happens. What next?")
+	hud.message(hud.AVATAR_DM, "<rabbit encounter here>")
 	yield(hud, "message_finished")
 	
 	hud.option("Begin the epic journey", self, "option_leave")
 	
 func option_leave():
-	get_tree().change_scene_to(NEXT_ENCOUNTER)
+	hud.message(hud.AVATAR_DM, "<write more stuff>")
