ebb4be0b8dc18fcee87d5ed525826ff656f5c80e
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-05-18 14:57:28 -0500

    Fill dialogue holes

--- project/campaigns/act1/7-rats.gd
+++ project/campaigns/act1/7-rats.gd
@@ -11,7 +11,10 @@ func _ready():
 func start_encounter():
 	#get_parent().connect("insert_signal_for_combat_finished_here", self, "combat_finished")
 	# Wait for message_clicked when more dialogue is queued
-	hud.message(hud.AVATAR_DM, "<rat encounter>")
+	hud.message(hud.AVATAR_DM, "Well, you're immediately greeted by the evil wizard's army of rats. Unfortunately I'm running out of time, so we're just going to skip this encounter.")
+	yield(hud, "message_clicked")
+	
+	hud.message(hud.AVATAR_WIZARD, "What!?")
 	yield(hud, "message_clicked")
 	
 	get_tree().change_scene_to(NEXT_ENCOUNTER)
